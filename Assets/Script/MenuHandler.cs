1a5a2e2a415c5be55e9e9e4873ce261710dfdad5
hnga-ng <hnga.nguyen01@gmail.com>
2026-04-15 15:04:35 +0300

    little UI

--- Assets/Script/MenuHandler.cs
+++ Assets/Script/MenuHandler.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // This is the important line!
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuHandler : MonoBehaviour
 {
@@ -7,5 +10,17 @@ public class MenuHandler : MonoBehaviour
     {
         // This loads the scene named "MainGame" from your list
         SceneManager.LoadScene("MainGame");
+    }
+
+    // Make sure this method is public and has no parameters so it appears in the Button OnClick() list
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        // Stop play mode when testing inside the Editor
+        EditorApplication.isPlaying = false;
+#else
+        // Quit the built application
+        Application.Quit();
+#endif
     }
 }
